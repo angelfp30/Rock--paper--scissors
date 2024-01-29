f04969961515080c2498ae66170b7fa84a427294
WINDOWS <156565466+angelfp30@users.noreply.github.com>
2024-01-29 15:17:35 -0500

    Creating computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+console.log("Welcome to Rock Paper Scissors, you'll play here in the console");
+let computerSelection;
+let playerSelection;
+
+function getComputerChoice(){
+    computerSelection = Math.floor(Math.random()*3);
+    if (computerSelection == 0){
+        computerSelection = "Rock"
+    } else if (computerSelection == 1){
+        computerSelection = "Paper"
+    } else {
+        computerSelection = "Scissors"
+    }
+    return computerSelection;
+}
+
+console.log(getComputerChoice(computerSelection));
