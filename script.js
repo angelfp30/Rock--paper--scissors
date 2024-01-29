2d630435a5306e048dcbeec1b81a745e247a0fc5
WINDOWS <156565466+angelfp30@users.noreply.github.com>
2024-01-29 18:13:09 -0500

    Coding the function to make the 5 round game, and calling it to play and display a result

--- script.js
+++ script.js
@@ -40,37 +40,81 @@ function getPlayerChoice(){
 //Stating the results of the previous functions
 getComputerChoice(computerSelection);
 getPlayerChoice(playerSelection);
+let winCount;
+let loseCount;
 
 //Conditions of the game
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "Rock" && computerSelection == "Paper") {
+        loseCount++
         return "You lose, paper wins to rock!";
 
     } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        winCount++
         return "You win, rock wins to scissors!"
 
     } else if (playerSelection == "Rock" && computerSelection == "Rock") {
         return "Both picked rock, it's a tie!"
 
     } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        winCount++
         return "You win, paper wins to rock!";
 
     } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        loseCount++
         return "You lose, scissors wins to paper!"
 
     } else if (playerSelection == "Paper" && computerSelection == "Paper") {
         return "Both picked paper, it's a tie!"
 
     } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        loseCount++
         return "You lose, rock wins to scissors!"
 
     } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        winCount++
         return "You win, scissors wins to paper!"
 
     } else if (playerSelection == "Scissors" && computerSelection == "Scissors"){
         return "Both picked scissors, it's a tie!"
     }
 }
+//Function to make the 5 round game
+function playGame(){
+        getComputerChoice();
+        getPlayerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
 
-//Final result
-console.log(playRound(playerSelection, computerSelection));
+        getComputerChoice();
+        getPlayerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+
+        getComputerChoice();
+        getPlayerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+
+        getComputerChoice();
+        getPlayerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+
+        getComputerChoice();
+        getPlayerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+    
+    
+    if (winCount > loseCount) {
+        console.log("You won!!!");
+    } else {
+        console.log("You lose :(");
+    }
+}
+//Calling the function
+playGame();
+
+
+
